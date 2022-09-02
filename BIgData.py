ed4f7b280fca59069fb9916c9da0e5ecc052d228
KabasCope <106401781+KabasCope@users.noreply.github.com>
2022-09-02 15:10:41 -0400

    Update BIgData.py

--- BIgData.py
+++ BIgData.py
@@ -30,3 +30,29 @@ for linea in archivo:
 # despliegue de los valores resultantes de la busqueda por comuna
 for c,v in comunas.items():
     print("Comuna: ",c, "Total: ",v)
+    
+=========================================================
+
+ ""Hacer este mismo codigo pero que el usuario pueda buscar la comuna con nombre""
+
+archivo = open("Microdato_Censo2017-Hogares.csv")
+nomb= open("microdato_censo2017-comunas.csv", encoding="UTF-8")
+dicComuna={} #Nuevo diccionario vacio
+#Creacion del diccionario con las claves y nombres de comuna
+for v in nomb:
+    v=v.rstrip()
+    v=v.split(";")
+    dicComuna[v[0]]=v[1]
+#print(dicComuna)
+# Filtrado de resultados a partir del codigo de region y tipo de hogar
+comunas={} #Diccionario vacio
+for linea in archivo:
+    linea = linea.rstrip()
+    linea = linea.split(";")
+    if linea[0] == "4": # Define el filtro de la region por numero, el que esta entre comillas define el buscador de comunas
+        if linea[9] == "2": #Tipo de hogar
+            comunas[linea[2]]=comunas.get(linea[2],0)+1
+#print(comunas)
+# despliegue de los valores resultantes de la busqueda por comuna
+for c,v in comunas.items():
+    print("Comuna:",dicComuna.get(c),"Total:",v)
