ccf067cdb302df53ed09865e21b380ab6982cf45
KabasCope <106401781+KabasCope@users.noreply.github.com>
2022-09-02 13:38:15 -0400

    Update BIgData.py

--- BIgData.py
+++ BIgData.py
@@ -6,8 +6,8 @@ def ejercicio1():
     for x in archivo:
         x=x.strip()             #Borra los caracteres especiales a la derecha de la linea
         x=x.split(";")          #Split borra los caracteres que esten dentro de ()
-        if x[0]=="9":             #0 en la lista es la region y 9 el tipo de hogar
-            if x[9]=="4":         #SI el tipo de hogar es el Nuclear biparental (4) en la lista de hogar tipo
+        if x[0]=="9":           #0 en la lista es la region y 9 el tipo de hogar, define el filtro de la region por numero
+            if x[9]=="4":       #SI el tipo de hogar es el Nuclear biparental (4) en la lista de hogar tipo, define el tipo de hogar
                 total +=1       #Lo mismo que total=total+1
     print("El total de hogares en la 9na region de tipo compuesto es: ",total)
 
